c28bd97b454091868ef65c74781e85ea28b8d657
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-03-16 23:08:41 -0400

    feat(gtk): create simple gtk app
    Link GTK4 as a system library and create a simple app using the C ABI.
    Requires some cleanup and probably should be done safer, but I am still
    learning so I have no idea what I'm doing.
    
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

---
+++ src/main.zig
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+const std = @import("std");
+
+const gtk = @cImport({
+    @cInclude("gtk/gtk.h");
+});
+
+pub fn main() !u8 {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const app: *gtk.GtkApplication = gtk.gtk_application_new("dev.elzody.eldur", gtk.G_APPLICATION_DEFAULT_FLAGS);
+    defer gtk.g_object_unref(app);
+
+    const status = gtk.g_application_run(@ptrCast(app), @intCast(args.len), @ptrCast(args));
+
+    return @intCast(status);
+}
