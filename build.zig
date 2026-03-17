631c4d882c225b65432423094a0132950340e4a4
Elizabeth Danzberger <elizabeth@elzody.dev>
2026-03-16 22:29:55 -0400

    chore: add spdx header
    Signed-off-by: Elizabeth Danzberger <elizabeth@elzody.dev>

--- build.zig
+++ build.zig
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0
+
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0
+
 const std = @import("std");
 
 const gtk = @cImport({
